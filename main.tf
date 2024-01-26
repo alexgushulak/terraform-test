e6418c56abc012d9a764fcda9de865618b61fb08
Alex Gushulak <alex.gushulak@gmail.com>
2024-01-26 15:53:10 -0500

    Updated AWS keys

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Key"
+}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = "A"
-  secret_key = "B"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 # 1. create VPC
